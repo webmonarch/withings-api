1523ca5c4d376840699f9320b8beedf21fa7e883
webmonarch <eric@collectivegenius.net>
2012-03-14 16:31:33 -0700

    symbols are better than strings

--- test/spec/types/measurement_type_spec.rb
+++ test/spec/types/measurement_type_spec.rb
@@ -5,7 +5,7 @@ describe Withings::Api::MeasurementType do
     subject { Withings::Api::MeasurementType }
 
     # ensure expected type constants are defined
-    ["WEIGHT", "HEIGHT", "FAT_MASS", "FAT_FREE_MASS", "FAT_RATIO"].each do |name|
+    [:WEIGHT, :HEIGHT, :FAT_MASS, :FAT_FREE_MASS, :FAT_RATIO].each do |name|
       it name do
         subject.const_get(name).should be
       end
