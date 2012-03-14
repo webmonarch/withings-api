2468cb8f683bd90cf413c3a6b955964b74bb93c0
webmonarch <eric@collectivegenius.net>
2012-03-14 16:36:28 -0700

    removing testing related constants from consts.rb

--- lib/withings-api/consts.rb
+++ lib/withings-api/consts.rb
@@ -5,9 +5,6 @@ module Withings
       OAUTH_REQUEST_TOKEN_PATH = "/account/request_token"
       OAUTH_AUTHORIZE_PATH = "/account/authorize"
       OAUTH_ACCESS_TOKEN_PATH = "/account/access_token"
-
-      TESTING_CONSUMER_KEY = "08943c64c3ccfe86d5edb40c8db6ddbbc43b6f58779c77d2b02454284db7ec"
-      TESTING_CONSUMER_SECRET = "85949316f07fc41346be145fe8fbc18b73f954f280be958033571720510"
     end
   end
 end
